fb12dcfc49a980167864d7883a0a6287a6f28e34
Lucas Urbas <lucas.urbas@gmail.com>
2018-05-24 12:13:51 -0700

    Add animations

--- app/src/main/java/com/lucasurbas/counter/ui/detail/widget/CounterView.kt
+++ app/src/main/java/com/lucasurbas/counter/ui/detail/widget/CounterView.kt
@@ -1,6 +1,8 @@
 package com.lucasurbas.counter.ui.detail.widget
 
 import android.content.Context
+import android.os.Parcel
+import android.os.Parcelable
 import android.util.AttributeSet
 import android.widget.FrameLayout
 import android.widget.TextView
@@ -13,6 +15,8 @@ import com.lucasurbas.counter.ui.main.di.MainActivityComponent
 import java.util.*
 import javax.inject.Inject
 
+
+
 class CounterView : FrameLayout, CounterViewPresenter.View {
 
     companion object {
@@ -21,17 +25,26 @@ class CounterView : FrameLayout, CounterViewPresenter.View {
 
     private var primaryTextSize: Float = 0F
     private var secondaryTextSize: Float = 0F
+    private var counterViewState: UiCounterViewState? = null;
 
     @BindView(R.id.minute_left_digit)
     lateinit var minuteLeftDigit: TextView
+    @BindView(R.id.minute_left_next_digit)
+    lateinit var minuteLeftNextDigit: TextView
     @BindView(R.id.minute_right_digit)
     lateinit var minuteRightDigit: TextView
+    @BindView(R.id.minute_right_next_digit)
+    lateinit var minuteRightNextDigit: TextView
     @BindView(R.id.minutes_divider)
     lateinit var minutesDivider: TextView
     @BindView(R.id.second_left_digit)
     lateinit var secondLeftDigit: TextView
+    @BindView(R.id.second_left_next_digit)
+    lateinit var secondLeftNextDigit: TextView
     @BindView(R.id.second_right_digit)
     lateinit var secondRightDigit: TextView
+    @BindView(R.id.second_right_next_digit)
+    lateinit var secondRightNextDigit: TextView
     @BindView(R.id.seconds_divider)
     lateinit var secondsDivider: TextView
     @BindView(R.id.millis_digits)
@@ -80,10 +93,14 @@ class CounterView : FrameLayout, CounterViewPresenter.View {
     private fun setupView() {
         if (primaryTextSize != DEFAULT) {
             minuteLeftDigit.textSize = primaryTextSize
+            minuteLeftNextDigit.textSize = primaryTextSize
             minuteRightDigit.textSize = primaryTextSize
+            minuteRightNextDigit.textSize = primaryTextSize
             minutesDivider.textSize = primaryTextSize
             secondLeftDigit.textSize = primaryTextSize
+            secondLeftNextDigit.textSize = primaryTextSize
             secondRightDigit.textSize = primaryTextSize
+            secondRightNextDigit.textSize = primaryTextSize
         }
         if (secondaryTextSize != DEFAULT) {
             secondsDivider.textSize = secondaryTextSize
@@ -103,15 +120,107 @@ class CounterView : FrameLayout, CounterViewPresenter.View {
 
     override fun render(counterViewState: UiCounterViewState) {
         counterViewState.value?.let {
-            minuteLeftDigit.text = counterViewState.minuteLeft.toString()
-            minuteRightDigit.text = counterViewState.minuteRight.toString()
-            secondLeftDigit.text = counterViewState.secondLeft.toString()
-            secondRightDigit.text = counterViewState.secondRight.toString()
+            if (counterViewState.minuteLeftAnimate) {
+                animateDigit(minuteLeftDigit, minuteLeftNextDigit, counterViewState.minuteLeft.toString())
+            } else if (minuteLeftDigit.tag == null) {
+                minuteLeftDigit.text = counterViewState.minuteLeft.toString()
+            }
+            if (counterViewState.minuteRightAnimate) {
+                animateDigit(minuteRightDigit, minuteRightNextDigit, counterViewState.minuteRight.toString())
+            } else if (minuteRightDigit.tag == null) {
+                minuteRightDigit.text = counterViewState.minuteRight.toString()
+            }
+            if (counterViewState.secondLeftAnimate) {
+                animateDigit(secondLeftDigit, secondLeftNextDigit, counterViewState.secondLeft.toString())
+            } else if (secondLeftDigit.tag == null) {
+                secondLeftDigit.text = counterViewState.secondLeft.toString()
+            }
+            if (counterViewState.secondRightAnimate) {
+                animateDigit(secondRightDigit, secondRightNextDigit, counterViewState.secondRight.toString())
+            } else if (secondRightDigit.tag == null) {
+                secondRightDigit.text = counterViewState.secondRight.toString()
+            }
             millisDigits.text = String.format(Locale.ROOT, "%02d", counterViewState.millis)
+
+            this.counterViewState = counterViewState
         }
+    }
+
+    fun animateDigit(digitView: TextView, digitNextView: TextView, stringValue: String) {
+        digitNextView.text = stringValue
+        digitNextView.animate()
+                .translationY(digitNextView.height.toFloat())
+                .alpha(1F)
+                .setDuration(300)
+                .withEndAction { resetNextView(digitNextView) }
+                .start()
+
+        digitView.tag = "Freeze"
+        digitView.animate()
+                .translationY(digitView.height.toFloat())
+                .alpha(0F)
+                .setDuration(300)
+                .withEndAction { resetView(digitView, stringValue) }
+                .start()
+    }
+
+    private fun resetView(textView: TextView, stringValue: String) {
+        textView.text = stringValue
+        textView.translationY = 0F
+        textView.alpha = 1F;
+        textView.tag = null
+    }
+
+    private fun resetNextView(textView: TextView) {
+        textView.translationY = 0F
+        textView.alpha = 0F;
     }
 
     fun setCounterValueInMillis(value: Long) {
         presenter.newValue(value)
+    }
+
+    public override fun onSaveInstanceState(): Parcelable? {
+        val savedState = SavedState(super.onSaveInstanceState())
+        savedState.counterViewState = counterViewState
+        return savedState
+    }
+
+    public override fun onRestoreInstanceState(state: Parcelable) {
+        if (state is SavedState) {
+            super.onRestoreInstanceState(state.superState)
+            state.counterViewState?.let { render(it) }
+        } else {
+            super.onRestoreInstanceState(state)
+        }
+    }
+
+    internal class SavedState : BaseSavedState {
+
+        var counterViewState: UiCounterViewState? = null
+
+        constructor(source: Parcel) : super(source) {
+            counterViewState = source.readParcelable(UiCounterViewState::class.java.classLoader)
+        }
+
+        constructor(superState: Parcelable) : super(superState)
+
+        override fun writeToParcel(out: Parcel, flags: Int) {
+            super.writeToParcel(out, flags)
+            out.writeParcelable(counterViewState, flags)
+        }
+
+        companion object {
+            @JvmField
+            val CREATOR = object : Parcelable.Creator<SavedState> {
+                override fun createFromParcel(source: Parcel): SavedState {
+                    return SavedState(source)
+                }
+
+                override fun newArray(size: Int): Array<SavedState?> {
+                    return arrayOfNulls(size)
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/lucasurbas/counter/ui/detail/widget/UiCounterViewState.kt
+++ app/src/main/java/com/lucasurbas/counter/ui/detail/widget/UiCounterViewState.kt
@@ -1,15 +1,34 @@
 package com.lucasurbas.counter.ui.detail.widget
 
+import android.os.Parcel
+import android.os.Parcelable
+import paperparcel.PaperParcel
 import java.util.concurrent.TimeUnit
 
+@PaperParcel
 data class UiCounterViewState(
         val value: Long? = null,
         val minuteLeft: Int = 0,
+        val minuteLeftAnimate: Boolean = false,
         val minuteRight: Int = 0,
+        val minuteRightAnimate: Boolean = false,
         val secondLeft: Int = 0,
+        val secondLeftAnimate: Boolean = false,
         val secondRight: Int = 0,
+        val secondRightAnimate: Boolean = false,
         val millis: Int = 0
-) {
+) : Parcelable {
+
+    companion object {
+        @JvmField val CREATOR = PaperParcelUiCounterViewState.CREATOR
+    }
+
+    override fun describeContents() = 0
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        PaperParcelUiCounterViewState.writeToParcel(this, dest, flags)
+    }
+
     interface Part {
 
         fun computeNewState(previousState: UiCounterViewState): UiCounterViewState
@@ -26,12 +45,21 @@ data class UiCounterViewState(
                 val secondRight = seconds % 10
                 val millis = valueInMillis % TimeUnit.SECONDS.toMillis(1) / 10
 
+                val minuteLeftAnimate = previousState.minuteLeft != minuteLeft.toInt()
+                val minuteRightAnimate = previousState.minuteRight != minuteRight.toInt()
+                val secondLeftAnimate = previousState.secondLeft != secondLeft.toInt()
+                val secondRightAnimate = previousState.secondRight != secondRight.toInt()
+
                 return previousState.copy(
                         value = valueInMillis,
                         minuteLeft = minuteLeft.toInt(),
+                        minuteLeftAnimate = minuteLeftAnimate,
                         minuteRight = minuteRight.toInt(),
+                        minuteRightAnimate = minuteRightAnimate,
                         secondLeft = secondLeft.toInt(),
+                        secondLeftAnimate = secondLeftAnimate,
                         secondRight = secondRight.toInt(),
+                        secondRightAnimate = secondRightAnimate,
                         millis = millis.toInt()
                 )
             }
